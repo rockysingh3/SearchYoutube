f89f1f77b6334c42d3bdbe427ec789d045541211
Rocky <rockygsingh@icloud.com>
2018-10-12 17:01:53 -0400

    set the state for the data fetched from yoututbe

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 
 // internal files
 import SearchBar from './Components/search_bar';
 import APIKEY from './helper/config';
 
+let term = 'fight';
+YTSearch({Key: APIKEY, term: term}, (data) => {
+  console.log(data);
+});
+
+
+
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { videos: [] }
+
+    YTSearch({Key: APIKEY, term: term}, (data) => {
+      this.setState({ videos: data });
+    });
+
+  }
+
   render(){
     return (
       <SearchBar/>

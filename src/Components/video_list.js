2369861503d3b40673d5357ca613e196d46044b0
Rocky <rockygsingh@icloud.com>
2018-10-12 19:06:24 -0400

    looped through list of the videos and rendered the over too video_listItem

--- src/Components/video_list.js
+++ src/Components/video_list.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import VideoListItem from './video_listItem';
 
+
+// displays list of all the videos 
 
 const VideoList = (props) => {
 
+    //console.log(props.videos);
+    const singleVideo = props.videos.map((singleVideo) => {
+        return <VideoListItem key={singleVideo.etag} singleVideo={singleVideo}/>
+    });
 
     return (
         <ul className="col-md-4 list-group">
-            {props.videos.length}
+            {singleVideo}
         </ul>
-    )
+    );
 
 }
 
